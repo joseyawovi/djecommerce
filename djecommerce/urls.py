5290a311b7132089d341f3411d4a6cf42fed1959
joseyawovi <136805772+joseyawovi@users.noreply.github.com>
2026-09-25 00:19:13 +0000

    I want to add the templates and statics

---
+++ djecommerce/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("", include("core.urls"),name="core"),
+    path('accounts/', include('allauth.urls')),
+    path("admin/", admin.site.urls),
+
+]
